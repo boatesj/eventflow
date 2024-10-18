ac0c06fe51e0493f32e2b2069af2230731fc70f1
Jake Boateng <64sjjsi0@students.codeinstitute.net>
2024-10-18 11:35:49 +0000

    added models.py and set the class event model

---
+++ eventflow/models.py
@@ -0,0 +1,19 @@
+from eventify import db  # Import from the correct app module
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    events = db.relationship('Event', backref='category', lazy=True)
+
+# Schema for the Event model
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), unique=True, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+    time = db.Column(db.String(8), nullable=False)
+    location = db.Column(db.String(200), nullable=False)
+    created_at = db.Column(db.DateTime, default=db.func.now(), nullable=False)
+    featured = db.Column(db.Boolean, default=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=True)
+    image_file = db.Column(db.String(120), nullable=True)
